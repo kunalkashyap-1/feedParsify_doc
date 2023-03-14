e5d02803b3a36503d14abd96581f777006579a05
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2023-03-14 21:58:59 +0530

    design and doc changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,33 +1,41 @@
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, NavDropdown, Nav } from 'react-bootstrap';
 
 function Header() {
     return (
         <Navbar expand="md">
             <Container fluid >
-            <Navbar.Brand >
-                <Link className="navbar-brand" to="/">FeedParsify</Link>
-            </Navbar.Brand>
-            
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" >
-                <Nav className="ms-auto ">
-                    <Nav >
-                        <Nav.Link className="anchor" href="/#guide">Quick guide</Nav.Link>
-                        <Nav.Link className="anchor" href="/#endpoint">Endpoints</Nav.Link>
-                        <Link className="anchor nav-link" to="/Console" >Console</Link>
-                        <Nav.Link className="anchor" href="/#example">Examples</Nav.Link>
-                        <Nav.Link className="anchor" href="https://github.com/kunalkashyap-1/RSS_feed_parser-scraper">Git hub</Nav.Link>
+                <Navbar.Brand >
+                    <Link className="navbar-brand" to="/">FeedParsify</Link>
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" >
+                    <Nav className="ms-auto ">
+                        <Nav >
+                        <NavDropdown title="Git hub" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="https://github.com/kunalkashyap-1/feedParsify_doc">
+                                    Frontend Docs
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="https://github.com/kunalkashyap-1/RSS_feed_parser-scraper">
+                                Backend API
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link className="anchor" href="/#guide">Quick guide</Nav.Link>
+                            <Nav.Link className="anchor" href="/#endpoint">Endpoints</Nav.Link>
+                            <Link className="anchor nav-link" to="/Console" >Console</Link>
+                            <Nav.Link className="anchor" href="/#example">Examples</Nav.Link>
+                            
+                        </Nav>
                     </Nav>
-                </Nav>
-            </Navbar.Collapse>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
 
-        
 
-        );
+
+    );
 }
 
 export default Header;
